9dc6fb98764b2e39b444896406856f44415d38c7
khaiphong <khaiphong.prajna@gmail.com>
2018-08-21 21:57:30 -0600

    serverless function in composable event

--- personadb.go
+++ personadb.go
@@ -19,40 +19,43 @@ import (
 
 type Bucket struct {
   // Bucket <-> Bucket | Test the serverlessFunction to JSON decode all event data of this Bucket
-  // label, tag, serverlessFunction  string
+  // length of B, T, P, label, tag, serverlessFunction  string
   ContextAtts map[string][]byte
-
   B [] Bucket
   T [] Topic
   P [] Event // legal permissions
 }
+
 // Registered topic _schema to dynamically change and update the Central Nervous System
 type Topic struct {
-  // Message <- Topic -> Event
-  // Test the serverlessFunction to JSON decode all event data of this Node
-  // label, tag, serverlessFunction  string
-  /*  
+  /*
+    Message <- Topic -> Event
+    Test the serverlessFunction to JSON decode all event data of this Node
+    length of B, T, O, label, tag, serverlessFunction  string
+    Producer publishes to a topic and emit a message. Consumer pulls from a topic
+
     Topics: filtered by place, actionTopic, qualifier -> users, orgnizations, services
     employ, employTeacher, enployNurse,
     eat, eatChinese, eatItalian,
     shop, shopGobal, shopCloth,
    */
   ContextAtts map[string][]byte
-
-  // Producer publishes to a topic. Consumer pulls from a topic
   B [] Bucket
   T [] Topic
   O [] Event // Occurance implies both Action and Relationship.
 }
+
 // use goroutines and channels for parallelism and concurrencies
 type Event struct { 
-  // use https://github.com/cloudevents/spec
-  // eventType, cloudEventsVersion, source, eventID, schemaURL, contentType  string
+  /* 
+    use https://github.com/cloudevents/spec
+    eventType, cloudEventsVersion, source, eventID, schemaURL, contentType  string
+    use "composableEvent" having (fn Event) value as serverless function in Extensions map
+    location, actionTopic, qualifier, SIC rating, composableEvent string. Initially, 
+    we can use label and tag and put all events in 1 OmHub. Break it out later into topic.
+  */
   ContextAtts map[string][]byte
-  // location, actionTopic, qualifier, SIC rating, serverlessFunction string. Initially, 
-  // we can use label and tag and put all events in 1 OmHub. Break it out later into topic.
   Extensions map[string][]byte
-
   Data map[string][]byte
 }
 
@@ -61,11 +64,14 @@ type Event struct {
 func dataExtraction (w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Securely Data extraction!")
 }
-
-// A serverless function, as result of PersonaAI Normative-Positive Intelligence,
-// to change the course of the event processes coming from the interaction of streaming
-// and crytographic systems of the Om Central Nervous System
-func composableEvent (w http.ResponseWriter, r *http.Request) {
+  /*
+    A serverless function fn, as result of PersonaAI Normative-Positive Intelligence, to
+    change the course of the event processes coming from the interaction of streaming and
+    crytographic systems of the Om Central Nervous System. The composableEvent takes the
+    passed (fn Event) to process.
+  */
+func composableEvent (w http.ResponseWriter, r *http.Request, fn Event) {
+    // process the passed fn Event
 	fmt.Fprintf(w, "Event Injection, Deletion, Massaging!")
 }
 
@@ -74,11 +80,6 @@ func composableEvent (w http.ResponseWriter, r *http.Request) {
 //}
 
 func main() {
-//    newBucket := make([] Bucket, 0, 1)
-//    newTopic := make([] Topic, 0, 1)
-//    newPermission := make([] Event, 0, 1)
-//    newOccurance := make([] Event, 0, 1)
-
     resp, err := http.Get("https://google.com")
     // check(err)
     body, err := ioutil.ReadAll(resp.Body)
@@ -100,5 +101,11 @@ func main() {
  	// the server must keep open
 	http.ListenAndServe(":8080", router)
 }
+
+    newBucket := make([] Bucket, 0, 1)
+    newTopic := make([] Topic, 0, 1)
+    newPermission := make([] Event, 0, 1)
+    newOccurance := make([] Event, 0, 1)
+
 */
 
