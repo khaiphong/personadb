083b77cf6e0e3b9808a8622285efd805411929f8
khaiphong <khaiphong.prajna@gmail.com>
2020-12-24 19:44:40 -0700

    React Hooks

---
+++ src/components/Navbar/NavbarElements.js
@@ -0,0 +1,114 @@
+import { NavLink as Link } from 'react-router-dom';
+import styled from 'styled-components';
+import './BtnBars.css';
+
+export const Nav = styled.nav`
+  background: #256ce1;
+  height: 36px;
+  top: 0;
+  left: 0;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  padding: 0.5rem calc((100vw - 1000px) / 2);
+  z-index: 10;
+
+  /* Third Nav */
+  /* justify-content: flex-start; */
+`;
+
+
+export const NavLink = styled(Link)`
+  color: #fff;
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  padding: 0 1rem;
+  height: 100%;
+  cursor: pointer;
+
+  &.active {
+    color: rgb(180, 126, 9);
+    font-size: 1.1rem;
+    font-weight: bold;
+    
+  }
+`;
+
+export const BtnBars = () => {
+  return (
+    <div>
+    <button className="toggle-button"  >
+      <div className="toggle-button__line" />
+      <div className="toggle-button__line" />
+      <div className="toggle-button__line" />
+    </button>
+    </div>
+  )
+}
+
+export const Bars = styled(BtnBars)`
+  display: none;
+  color: #fff;
+  @media screen and (max-width: 768px) {
+    display: block;
+    position: absolute;
+    top: 0;
+    right: 0;
+    transform: translate(-100%, 75%);
+    font-size: 1.8rem;
+    cursor: pointer;
+  }
+`;
+
+export const NavMenu = styled.div`
+  display: flex;
+  align-items: center;
+  margin-right: -24px;
+
+  /* Second Nav */
+  /* margin-right: 24px; */
+
+  /* Third Nav */
+  /* width: 100vw;
+  white-space: nowrap; */
+
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
+
+export const NavBtn = styled.nav`
+  display: flex;
+  align-items: center;
+  margin-right: 24px;
+
+  /* Third Nav */
+  /* justify-content: flex-end;
+  width: 100vw; */
+
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
+
+export const NavBtnLink = styled(Link)`
+  border-radius: 4px;
+  background: #256ce1;
+  color: #fff;
+  outline: none;
+  border: none;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+  text-decoration: none;
+
+  /* Second Nav */
+  margin-left: 24px;
+
+  &:hover {
+    transition: all 0.2s ease-in-out;
+    background: #fff;
+    color: #010606;
+  }
+`;
+
