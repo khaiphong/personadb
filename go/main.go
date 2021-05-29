80ba5311de4e154ce6ed34b43c515d35f493ae31
khaiphong <khaiphong.prajna@gmail.com>
2021-05-29 08:31:38 -0600

    embedded BadgerDB

--- go/main.go
+++ go/main.go
@@ -9,9 +9,8 @@
   Unless required by applicable law or agreed to in writing, software
   distributed under the License is distributed on an "AS IS" BASIS,
   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-
-PersonaDB is a microservice to turn Badger() embedded db into a custom personalized data store via FP (functional programming), plus value-added in GitOp and Prometheus time series. The underlying BadgerDB is (1) wrapped with distriuted graph db in operation, Node-Solid-Server for pod (personal online data) sharing CD/CI via https://github.com/google/ko for executable image.
-Ref: fetching data Storing data in Dgraph: https://dgraph.io/blog/post/building-todo-list-react-dgraph/
+  
+PersonaDB is an injected microservice of KP-Platform for Data Modeling, Simulation and Visualization, plus GitOp modules to enable other independent developers value-add their services using PersonaDB services to access the user's private database of private and public data in their agreed smart professional contracts.
 */
 
 package main
@@ -39,4 +38,21 @@ func main() {
 func run() {
   fmt.Printf("Running %v as user %d in process %d\n", os.Args[2:], os.Geteuid(), os.Getpid())
 }
+
+func Bucket() {
+  fmt.Printf("Bucket features on Badger")
+}
+
+func Bees() {
+  fmt.Printf("Business Economics Engineering System")
+}
+
+func Prometheus() {
+  fmt.Printf("Prometheus on Badger")
+}
+
+func GitOp() {
+  fmt.Printf("GitOp on Badger")
+}
+
 
